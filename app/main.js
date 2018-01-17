e3a63302bc4164b37f12b544d4d245ccf8384d7f
tanglimei <tanglimei@mininglamp.com>
2018-01-17 11:01:10 +0800

    tijiap

---
+++ app/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './Greeter';
+
+import './main.css';
+// import './tests.scss';
+
+render(<Greeter />,document.getElementById('root'));
